d6080f80f90209f689c05518e49b8f8abd503ccc
Adam Gaia <git@agaia.dev>
2022-08-26 16:02:26 -0600

    init

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Bats-support helper library";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils, ...}:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in rec {
+        bats-support = pkgs.stdenv.mkDerivation {
+          name = "bats-support";
+          src = pkgs.fetchgit {
+            url = "https://github.com/bats-core/bats-support.git";
+            rev = "v0.3.0";
+            sha256 = "sha256-4N7XJS5XOKxMCXNC7ef9halhRpg79kUqDuRnKcrxoeo=";
+          };
+          installPhase = ''
+            mkdir -p $out/bin
+            cp ./load.bash $out/bin/
+            cp -r ./src $out/bin/
+          '';
+        };
+        packages = { default = bats-support; };
+      });
+}
